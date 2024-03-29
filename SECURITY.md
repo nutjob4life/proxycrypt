60749d5fd4066591db82403f76407c0d40c4e4ae
Sean Kelly <kelly@seankelly.biz>
2024-03-29 15:33:31 -0500

    Make own

--- SECURITY.md
+++ SECURITY.md
@@ -1,18 +1,3 @@
 # 👮‍♀️ Security Policy
 
-This document describes the security policy of ProxyCrypt.
-
-
-## 📀 Supported Versions
-
-The following versions are supported and/or unsupported at this time:
-
-| Version | Supported |
-|:--------|:---------:|
-| 1.0.0   | ✅        |
-| <1.0    | ❌        |
-
-
-## 🚨 Reporting a Vulnerability
-
-To report a vulnerability, [please submit an issue on our tracker](https://github.com/NASA-PDS/proxycrypt/issues/new?template=vulnerability-issue.md).
+There is no security policy.
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,35 +2,6 @@
 #
 # 🕵️‍♀️ ProxyCrypt entrypoint — This is invoked at container startup.
 #
-# Copyright © 2022, California Institute of Technology ("Caltech").
-# U.S. Government sponsorship acknowledged.
-#
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# • Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-# • Redistributions must reproduce the above copyright notice, this list of
-#   conditions and the following disclaimer in the documentation and/or other
-#   materials provided with the distribution.
-# • Neither the name of Caltech nor its operating division, the Jet Propulsion
-#   Laboratory, nor the names of its contributors may be used to endorse or
-#   promote products derived from this software without specific prior written
-#   permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
 #
 # Check essential env vars:
 : ${PROXY_URL:?✋ The environment variable PROXY_URL is required}
@@ -46,6 +17,13 @@ echo "💁‍♀️ The CERT_CN is ${CERT_CN}" 1>&2
 /usr/bin/openssl req -nodes -x509 -days ${CERT_DAYS} -newkey rsa:2048 \
     -keyout /etc/ssl/self.key -out /etc/ssl/self.cert \
     -subj "/C=US/ST=California/L=Pasadena/O=Caltech/CN=${CERT_CN}"
+
+if [ "$WHITELIST"x != "x" ]; then
+    /whitelist.sh
+else
+    cp /dev/null /etc/nginx/whitelist.conf
+fi
+
 
 # Turn things over to Nginx
 exec /docker-entrypoint.sh "$@"
