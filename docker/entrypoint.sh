31ef3e0c1d17f3150f10379f83e4d11568987012
Sean Kelly <kelly@seankelly.biz>
2024-03-30 17:09:38 -0500

    Remove extraneous logging, cosmetic comments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,18 +6,20 @@
 # Check essential env vars:
 : ${PROXY_URL:?✋ The environment variable PROXY_URL is required}
 
+
 # "Reasonable" defaults:
 CERT_CN=${CERT_CN:-localhost}
 CERT_DAYS=${CERT_DAYS:-365}
 
-echo "💁‍♀️ The CERT_CN is ${CERT_CN}" 1>&2
 
 # Make a random self-signed certificate
 /bin/rm -f /etc/ssl/self.key /etc/ssl/self.cert
 /usr/bin/openssl req -nodes -x509 -days ${CERT_DAYS} -newkey rsa:2048 \
     -keyout /etc/ssl/self.key -out /etc/ssl/self.cert \
-    -subj "/C=US/ST=California/L=Pasadena/O=Caltech/CN=${CERT_CN}"
+    -subj "/C=US/ST=Texas/L=Crafty Stream/O=Minecraft/CN=${CERT_CN}"
 
+
+# Handle whitelisting, if any
 if [ "$WHITELIST"x != "x" ]; then
     /whitelist.sh
 else
